69b6c63afcc47ae47cccbb94b5fd09018fa780ff
pevargasg <pevargas@valdosta.edu>
2016-11-28 18:42:26 -0800

    routes

--- app/app.routing.ts
+++ app/app.routing.ts
@@ -4,3 +4,15 @@ import {Routes, RouterModule} from '@angular/router';
 import {SearchComponent} from './components/search/search.component';
 import {AboutComponent} from './components/about/about.component';
 
+const appRoutes: Routes = [
+    {
+        path:'',
+        component: SearchComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
