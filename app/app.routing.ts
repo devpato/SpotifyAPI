ebc07b65f2f23a8c97a68b99f145635d66e4f23c
pevargasg <pevargas@valdosta.edu>
2016-11-28 18:33:54 -0800

    routes

---
+++ app/app.routing.ts
@@ -0,0 +1,6 @@
+import {ModuleWithProviders} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {SearchComponent} from './components/search/search.component';
+import {AboutComponent} from './components/about/about.component';
+
