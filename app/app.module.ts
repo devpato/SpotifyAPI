0f2d9441cfb85c656f10201b659fdede61178a47
Patricio Vargas <pevargas@valdosta.edu>
2016-12-03 20:26:07 -0500

    link to artist profile

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,8 +5,10 @@ import { AppComponent }  from './app.component';
 import {SearchComponent} from './components/search/search.component'
 import {NavbarComponent} from './components/navbar/navbar.component'
 import {AboutComponent} from './components/about/about.component'
+import {ArtistComponent} from './components/artist/artist.component';
 import {HttpModule} from '@angular/http';
 import {routing} from './app.routing';
+
 
 @NgModule({
   imports:      [ BrowserModule,
@@ -16,7 +18,9 @@ import {routing} from './app.routing';
   declarations: [ AppComponent,
                   SearchComponent,
                   NavbarComponent,
-                  AboutComponent ],
+                  AboutComponent,
+                  ArtistComponent
+                   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- app/services/spotify.service.ts
+++ app/services/spotify.service.ts
@@ -5,7 +5,8 @@ import 'rxjs/add/operator/map';// to convert to json object
 @Injectable()
 export class SpotifyService{
     private searchUrl: string;
-    
+    private artistUrl: string;
+
     constructor(private _http:Http){
         
     }
@@ -14,5 +15,10 @@ export class SpotifyService{
         this.searchUrl = 'https://api.spotify.com/v1/search?query='+str+'&offset=0&limit=20&type='+type+'&market=US';
         return this._http.get(this.searchUrl)
             .map(res => res.json());
+    }
+    getArtist(id:string){
+        this.artistUrl = 'https://api.spotify.com/v1/artist/'+id;
+        return this._http.get(this.artistUrl)
+        .map(res => res.json());
     }
 }
